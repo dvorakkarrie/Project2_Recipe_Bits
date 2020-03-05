aaccce17a4a94531ed0bf93f18f766585bdbeb2e
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-05 10:55:29 -0600

    updated code to clear the input text box value and added comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import React, { useEffect, useState} from 'react';
 import React, { Component } from 'react';
 // import {Route, Link, Switch} from 'react-router-dom';
+
 // Imported axios functionality
 import axios from 'axios';
-// import dotenv to secure API
 
 //Import components
 import RecipeList from './RecipeList'
@@ -24,33 +24,29 @@ class App extends Component {
   constructor(props) {
     super(props)
       this.state = {
-        recipes: [],
-        search: null,
-        currentRecipe: null
+        recipes: [],                     // This will display recipes from the Edamam API call.
+        searchText: '',                  // This will hold search criteria entered by the user.
+        currentRecipe: null              // This will hold the recipe selected from the recipes array.
       }
   }
 
-  handleChange = (event) => {
-    console.log('handleChange is running')
+  handleChange = (event) => {            // This function will update the search variable.
     this.setState({
-      search: event.target.value})
-    console.log('Word is ' + this.state.search)
+      searchText: event.target.value})   // This will assign the user's entered data to the search variable.
   }
 
-  handleSubmit = (event) => {
-    console.log('handleSubmit is running')  
-    event.preventDefault()
-    console.log(this.state.search)
-    this.getRecipes()
+  handleSubmit = (event) => {           // This will trigger the API call to get recipes based upon searchRecipe value and subsequently clear the value.
+    event.preventDefault()              // This will prevent this function from refreshing.
+    this.getRecipes()                   // This will trigger the getRecipe function to send an API call to Edamam.
     this.setState({
-      searchRecipes: ''
+      searchText: ''                    // This will assign an empty string to the search variable.
     })
   }
 
-  getRecipes = () => {
+  getRecipes = () => {                  // This send the API call to Edamam to retrieve the requested recipes.
     axios({
       method: 'GET',
-      url: `https://api.edamam.com/search?q=${this.state.search}&app_id=${recipeID}&app_key=${recipeKey}&from=0&to=10&calories=591-722&health=alcohol-free`
+      url: `https://api.edamam.com/search?q=${this.state.searchText}&app_id=${recipeID}&app_key=${recipeKey}&from=0&to=10&calories=591-722&health=alcohol-free`
     })
     .then(response => {
       console.log(response.data.hits)
@@ -63,41 +59,39 @@ class App extends Component {
     })
   }
 
-  handleClick = (recipe) => {
-    console.log('handleClick is running')
-    console.log(recipe)
+  handleClick = (recipe) => {           // This will display only one selected recipe.
     this.setState({
-      currentRecipe: recipe
+      currentRecipe: recipe             // This will assign the selected recipe to the currentRecipe variable.
     })
   }
 
-render() {
-  return (
-    <div className="App">
-      <h1>Recipe Bits</h1>
-      <nav>Home</nav>
-      <form className='search-form' >
-        <input type='text' className='search-box' 
-          placeholder="i.e. chicken, ice cream" onChange={this.handleChange} 
-          value={this.searchRecipe}></input>         
-        <button type='button' className='search-button' 
-          onClick={this.handleSubmit}>Search
-        </button>        
-      </form>
-        {this.state.recipes && this.state.recipes.map((item, index) => (
-          <div key={index} className='search-results'>
-            <RecipeList 
-              title={item.recipe.label}
-              calories={item.recipe.calories}
-              image={item.recipe.image}
-              ingredients={item.recipe.ingredients}
-            />
-          </div>
-        )
-      )}
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <h1>Recipe Bits</h1>
+        <nav>Home</nav>
+        <form className='search-form' >
+          <input type='text' className='search-box' 
+            placeholder="i.e. chicken, ice cream" onChange={this.handleChange} 
+            value={this.state.searchText}>{this.state.value}</input>         
+          <button type='button' className='search-button' 
+            onClick={this.handleSubmit}>Search
+          </button>        
+        </form>
+          {this.state.recipes && this.state.recipes.map((item, index) => (
+            <div key={index} className='search-results'>
+              <RecipeList 
+                title={item.recipe.label}
+                calories={item.recipe.calories}
+                image={item.recipe.image}
+                ingredients={item.recipe.ingredients}
+              />
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
 }
  
 export default App;
